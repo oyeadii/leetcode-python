f6debcffe7bf2662a344d60368aa9a1a8303375b
Aditya Gupta <80537916+oyeadii@users.noreply.github.com>
2022-02-05 21:50:13 +0530

    Create 1299_replaceElements.py

---
+++ 1299_replaceElements.py
@@ -0,0 +1,9 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        if len(arr) == 1:
+            return [-1]
+        for i in range(len(arr)-1):
+            arr[i] = max(arr[i+1:len(arr)])
+        if len(arr)>1:
+            arr[len(arr)-1] = -1
+            return arr
